814f357bdb45f3a220bebf793f1ee3eed0068de1
@니메 <63767853+ttiah@users.noreply.github.com>
2024-03-21 23:54:26 +0900

    2024-03-21

---
+++ code_challenge/extractors/berlinstartupjobs.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+def extract_berlinstartup_jobs(keyword):
+    jobs = [{'title': 'Python 트레이딩 플랫폼 엔지니어 for VegaX', 'company': '콘스텔레이션코리아', 'reward': '합격보상금 100만원', 'link': 'https://www.wanted.co.kr/wd/193356'}]
+    return jobs
